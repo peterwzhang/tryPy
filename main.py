2941822d610e5ae98540c691ebbfd93ec089a67a
Madeline Moore <mcmoore11@crimson.ua.edu>
2022-04-29 10:40:33 -0500

    more formatting

--- main.py
+++ main.py
@@ -92,16 +92,16 @@ def main():
     screen = setup_screen(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
     tryPy_manager = BlockManager(screen)
 
-    tryPy_manager.add_block(Start(0, 0, 100, 50))
+    tryPy_manager.add_block(Start(0, 0, 200, 50))
     for i in range(5):
         #tryPy_manager.add_block(Block(0, 0, 100, 100))
-        tryPy_manager.add_block(If(0, 50, 100, 75))
-        tryPy_manager.add_block(Else(0, 125, 100, 75))
-        tryPy_manager.add_block(While(0, 200, 100, 75))
-        tryPy_manager.add_block(For(0, 275, 100, 75))
-        tryPy_manager.add_block(Print(0, 350, 100, 75))
-        tryPy_manager.add_block(Var(0, 425, 100, 50))
-        tryPy_manager.add_block(Break(0, 475, 100, 50))
+        tryPy_manager.add_block(If(0, 50, 200, 75))
+        tryPy_manager.add_block(Else(0, 125, 200, 75))
+        tryPy_manager.add_block(While(0, 200, 200, 75))
+        tryPy_manager.add_block(For(0, 275, 200, 75))
+        tryPy_manager.add_block(Print(0, 350, 200, 75))
+        tryPy_manager.add_block(Var(0, 425, 200, 50))
+        tryPy_manager.add_block(Break(0, 475, 200, 50))
 
     # variable for main game loop + mouse drag handling
     running = True
